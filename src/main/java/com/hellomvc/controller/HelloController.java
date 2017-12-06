3292bf12b44ddfaa4fa0cb1ac8523547f52f0577
user <user@user-TOSH.home>
2017-12-06 19:52:31 +0100

    My First Commit

---
+++ src/main/java/com/hellomvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.hellomvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello() {
+		
+		return "hello";
+	}
+
+}
